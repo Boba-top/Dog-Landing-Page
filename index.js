509093315d88433463ad015cac5bd4d36445e727
Ion758-creator <ionsaptefrati87@gmail.com>
2026-04-07 09:01:51 +0300

    Modificări locale înainte de pull

---
+++ index.js
@@ -0,0 +1,47 @@
+function searchDog() {
+    const breedValue = document.getElementById("searchInput").value.toLowerCase().trim();
+    const BREED_URL = "https://dog.ceo/api/breed/" + breedValue + "/images/random";
+
+    fetch(BREED_URL)
+        .then(res => res.json())
+        .then(data => {
+            if (data.status === "success") {
+                const dogImage = document.createElement("img");
+                dogImage.setAttribute("src", data.message);
+                dogImage.setAttribute("alt", breedValue);
+                dogImage.setAttribute("class", "breed-image");
+
+                const breedSection = document.getElementById("breed-section");
+                breedSection.innerHTML = "";
+                breedSection.appendChild(dogImage);
+            } else {
+                alert("Rasa nu există ");
+            }
+        })
+        .catch(err => {
+            alert("A apărut o eroare ");
+            console.log(err);
+        });
+}
+const themeBtn = document.getElementById("theme-button");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    const isDark = document.body.classList.contains("dark-mode");
+
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+
+    themeBtn.textContent = isDark ? "☀️" : "🌑";
+});
+
+window.addEventListener("load", () => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+        themeBtn.textContent = "☀️";
+    } else {
+        themeBtn.textContent = "🌑";
+    }
+});
